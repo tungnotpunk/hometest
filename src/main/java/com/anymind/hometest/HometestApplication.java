506b7db11c3455f686dff99edb7b480652127895
tungnotpunk <tungnotpunk@gmail.com>
2022-12-12 23:55:37 +0900

    added grpc-java example code for testing grpc with proto works

--- src/main/java/com/anymind/hometest/HometestApplication.java
+++ src/main/java/com/anymind/hometest/HometestApplication.java
@@ -1,13 +1,66 @@
 package com.anymind.hometest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.logging.Logger;
 
-@SpringBootApplication
+
 public class HometestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HometestApplication.class, args);
-	}
+  private static final Logger logger = Logger.getLogger(HometestApplication.class.getName());
 
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 50051;
+    server = ServerBuilder.forPort(port)
+        .addService(new GreeterImpl())
+        .build()
+        .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        HometestApplication.this.stop();
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  private void stop() {
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final HometestApplication server = new HometestApplication();
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+      logger.info(req.getName() + " come to say hi");
+      HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+  }
 }
