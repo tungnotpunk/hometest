4767b114277e5c27e43270d4be3866da1a84d30b
tungnotpunk <tungnotpunk@gmail.com>
2022-12-13 03:24:47 +0900

    updated record parser, added util files

--- src/main/java/com/anymind/hometest/payment/pointsystem/parser/RecordParser.java
+++ src/main/java/com/anymind/hometest/payment/pointsystem/parser/RecordParser.java
@@ -3,12 +3,26 @@ package com.anymind.hometest.payment.pointsystem.parser;
 import java.util.Date;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import java.text.ParseException;
 import com.anymind.hometest.payment.pointsystem.grpc.*;
+import com.anymind.hometest.payment.pointsystem.util.*;
 import com.anymind.hometest.payment.pointsystem.model.input.*;
 
 public class RecordParser {
 
   public static Record parse(RecordRequest req) {
-    return new Record();
+    Record input = new Record();
+    try {
+      input.setStartDateTime(DateUtil.datetimeWithTimezone(req.getStartDateTime()));
+    } catch (ParseException e) {
+      input.setError(Status.FAILED_PRECONDITION.withDescription("StartDateTime is not incorrect").asRuntimeException());
+      return input;
+    }
+    try {
+      input.setEndDateTime(DateUtil.datetimeWithTimezone(req.getEndDateTime()));
+    } catch (ParseException e) {
+      input.setError(Status.FAILED_PRECONDITION.withDescription("EndDateTime is not incorrect").asRuntimeException());
+    }
+    return input;
   }
 }
---
+++ src/main/java/com/anymind/hometest/payment/pointsystem/util/DateUtil.java
@@ -0,0 +1,16 @@
+package com.anymind.hometest.payment.pointsystem.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.text.ParseException;
+
+public class DateUtil {
+
+  public static Date datetimeWithTimezone(String datetimeWithTimezone) throws ParseException {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ssZ");
+    sdf.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
+    return sdf.parse(datetimeWithTimezone);
+  }
+}
