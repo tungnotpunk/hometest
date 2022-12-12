f3ac25426c708cee76933892bbe682c0b3d4807e
tungnotpunk <tungnotpunk@gmail.com>
2022-12-12 23:28:59 +0900

    initial of spring boot

---
+++ src/main/java/com/anymind/hometest/HometestApplication.java
@@ -0,0 +1,13 @@
+package com.anymind.hometest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anymind/hometest/HometestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anymind.hometest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
