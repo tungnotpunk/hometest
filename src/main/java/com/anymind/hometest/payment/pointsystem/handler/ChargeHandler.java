6982b1dc39f942616585eff31ef6092f919f711c
tungnotpunk <tungnotpunk@gmail.com>
2022-12-13 15:05:20 +0900

    updated protobuf types, added charged model, charge handler logic, updated pox jpa dependency

--- src/main/java/com/anymind/hometest/payment/pointsystem/handler/ChargeHandler.java
+++ src/main/java/com/anymind/hometest/payment/pointsystem/handler/ChargeHandler.java
@@ -4,26 +4,43 @@ import java.util.logging.Logger;
 import io.grpc.stub.StreamObserver;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.anymind.hometest.payment.pointsystem.parser.*;
 import com.anymind.hometest.payment.pointsystem.grpc.*;
+import com.anymind.hometest.payment.pointsystem.repo.*;
 import com.anymind.hometest.payment.pointsystem.model.input.*;
+import com.anymind.hometest.payment.pointsystem.model.output.*;
+import com.anymind.hometest.payment.pointsystem.model.db.ChargeRecord;
 
+@Component
 public class ChargeHandler {
 
   private static final Logger logger = Logger.getLogger(ChargeHandler.class.getName());
+  private static ChargeRecordRepository chargeRecordRepository;
 
+  @Autowired
+  public void setChargeRecordRepository(ChargeRecordRepository chargeRecordRepository){
+      ChargeHandler.chargeRecordRepository = chargeRecordRepository;
+  }
 
   public static void handle(ChargeRequest req, StreamObserver<ChargeReply> responseObserver) {
     try {
-        Charge input = ChargeParser.parse(req);
-        if (input.getError() != null) {
-            throw input.getError();
-        }
-        ChargeReply reply = ChargeReply.newBuilder().build();
-        responseObserver.onNext(reply);
-        responseObserver.onCompleted();
+      Charge input = ChargeParser.parse(req);
+      if (input.getError() != null) {
+        throw input.getError();
+      }
+      Charged output = new Charged(input.getPrice(), input.getPriceModifier(), input.getPaymentMethod().getPointRate());
+      ChargeRecord chargeRecord = new ChargeRecord(input, output);
+      chargeRecordRepository.save(chargeRecord);
+      ChargeReply reply = ChargeReply.newBuilder()
+      .setFinalPrice(output.getFinalPriceInString())
+      .setPoints(output.getPoints())
+      .build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
     } catch (StatusRuntimeException e) {
-        responseObserver.onError(e);
+      responseObserver.onError(e);
     }
   }
 }
---
+++ src/main/java/com/anymind/hometest/payment/pointsystem/model/output/Charged.java
@@ -0,0 +1,21 @@
+package com.anymind.hometest.payment.pointsystem.model.output;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+import com.anymind.hometest.payment.pointsystem.util.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Charged {
+  public Charged(double price, double priceModifier, double pointRate) {
+      this.finalPrice = price * priceModifier;
+      this.finalPriceInString = NumberUtil.doubleToTextWithDecimals(this.finalPrice, 2);
+      this.points = Math.floor(price * pointRate);
+  }
+  private double finalPrice;
+  private String finalPriceInString;
+  private double points;
+}
