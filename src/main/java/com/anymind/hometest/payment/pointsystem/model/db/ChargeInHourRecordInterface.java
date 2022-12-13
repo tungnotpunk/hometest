f975033c3db46263fc2eade1e04ec565a5973571
tungnotpunk <tungnotpunk@gmail.com>
2022-12-13 16:48:28 +0900

    added group function for select data

---
+++ src/main/java/com/anymind/hometest/payment/pointsystem/model/db/ChargeInHourRecordInterface.java
@@ -0,0 +1,8 @@
+package com.anymind.hometest.payment.pointsystem.model.db;
+
+public interface ChargeInHourRecordInterface {
+
+  String getDatetime();
+  double getSales();
+  double getPoints();
+}
--- src/main/java/com/anymind/hometest/payment/pointsystem/repo/ChargeRecordRepository.java
+++ src/main/java/com/anymind/hometest/payment/pointsystem/repo/ChargeRecordRepository.java
@@ -1,14 +1,29 @@
 package com.anymind.hometest.payment.pointsystem.repo;
 
-import com.anymind.hometest.payment.pointsystem.model.db.ChargeRecord;
 
+import java.util.List;
+import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
+import com.anymind.hometest.payment.pointsystem.model.db.*;
+import com.anymind.hometest.payment.pointsystem.model.output.*;
 
 @Repository("ChargeRecordRepository")
 public interface ChargeRecordRepository extends JpaRepository<ChargeRecord, Long> {
+
+  @Query(nativeQuery = true, value=
+    "SELECT " + 
+    "datetime_in_hour AS `datetime`, " +
+    "SUM(final_price) AS sales, " + 
+    "SUM(points) AS points " +
+    "FROM anymind.charge_record " +
+    "WHERE `datetime` BETWEEN :startDateTime AND :endDateTime " +
+    "GROUP BY datetime_in_hour " +
+    "ORDER BY datetime_in_hour ASC"
+  )
+  List<ChargeInHourRecordInterface> findGroupByDatetimeInHour(@Param("startDateTime") Date startDateTime, @Param("endDateTime") Date endDateTime);
 
 }
